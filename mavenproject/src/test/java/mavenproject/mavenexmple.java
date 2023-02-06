85e039f71bbdf6ae024a4d9830fadb99c461ea7e
HP BS 661 <HP BS 661@LAPTOP-JKFQ777J>
2023-02-06 13:12:38 +0530

    hi bro

---
+++ mavenproject/src/test/java/mavenproject/mavenexmple.java
@@ -0,0 +1,17 @@
+package mavenproject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class mavenexmple 
+{
+	public static void main(String[] args) 
+	{
+		WebDriverManager.firefoxdriver().setup();
+		WebDriver driver=new FirefoxDriver();
+		driver.get("https://www.amazon.in");
+		driver.quit();	
+		}
+}
